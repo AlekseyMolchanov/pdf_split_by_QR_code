8acf1aa001901f2e241a483dc7765abe3d5deba4
Aleksey Molchanov <molchanovav@gmail.com>
2019-03-05 02:05:26 +0300

    fix test to use PDFDocument as reader

--- split_test.py
+++ split_test.py
@@ -5,6 +5,7 @@ import os
 import pytest
 
 from tool import Tool
+from readers import PDFDocument
 
 INPUT_FILE_NULL = None
 INPUT_FILE_INVALID = 'invalid.pdf'
@@ -20,18 +21,18 @@ FILES_COUNT = 4
 
 
 def test_wand_pdf_to_png():
-    
+
     from wand.image import Image
-    
+
     with open(FILE_IMAGE_PNG, 'wb') as out:
         with Image(filename=INPUT_FILE_VALID, resolution=300) as img:
-            img.compression_quality = 100 
+            img.compression_quality = 100
             img.format = 'png'
             img.save(file=out)
-    
+
     with open(FILE_IMAGE_PNG, 'rb') as test:
         assert test.read()
-         
+
 
 # 1. Input
 
@@ -47,27 +48,31 @@ def test_null_input_file():
     # 1. Assert exception if input.file == null or empty
     #
     with pytest.raises(ValueError, match=r'Source is not set'):
-        tool = Tool(INPUT_FILE_NULL)
+        reader = PDFDocument(INPUT_FILE_NULL)
+        tool = Tool(reader)
 
 def test_not_PDF_input_file():
     #
     # 2. Assert exception if input.file != filetype pdf
     #
     with pytest.raises(ValueError, match=r'Is not PDF .*'):
-        tool = Tool(INPUT_FILE_INVALID)
+        reader = PDFDocument(INPUT_FILE_INVALID)
+        tool = Tool(reader)
 
 def test_PDF_input_file():
     #
     # 2. Assert exception if input.file == filetype pdf
     #
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     assert tool
-    
+
 def test_input_file_page_count():
     #
     # 3. Assert exception if input.pdf.page.size != 6
     #
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     assert tool.pages_count == PAGES_COUNT
     assert len(tool.pages) == PAGES_COUNT
 
@@ -75,14 +80,16 @@ def test_input_file_qrcodes_count():
     #
     # 4. Assert exception if input.file.qrcode.count < 1
     #
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     assert len(tool.qrcodes)
 
 def test_input_file_each_qrcode_is_valid_path():
     #
     # 5. Assert exception for each if input.file.qrcode != file path format
     #
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     for qrcode in tool.qrcodes:
         assert qrcode in FOLDERS
 
@@ -93,7 +100,8 @@ def test_each_output_directories_fullpath_is_one_of_input_qrcode_value():
     #
     # 1. Assert for each output.directories.fullpath is one of input.qrcode.value[]
     #
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     for each in tool.files:
         assert each.folder in FOLDERS
 
@@ -102,7 +110,8 @@ def test_output_directories_count():
     # 2. Assert output.directories.count == 3
     #
     directories = []
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     for each in tool.files:
         if each.folder not in directories:
             directories.append(each.folder)
@@ -113,7 +122,8 @@ def test_output_files_count():
     # 3. Assert output.files.count == 4
     #
     directories = []
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     assert len(tool.files) == FILES_COUNT
 
 def test_output_files_name():
@@ -121,6 +131,7 @@ def test_output_files_name():
     # 4. Assert each output.file.name == input.file.name+_+uuid
     #
     input_file_name = INPUT_FILE_VALID.split('.').pop(0)
-    tool = Tool(INPUT_FILE_VALID)
+    reader = PDFDocument(INPUT_FILE_VALID)
+    tool = Tool(reader)
     for each in tool.files:
         assert (each.file_name == '{}_{}.pdf'.format(input_file_name, each.uuid))
